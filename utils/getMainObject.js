aaacd5b7054406e39841a4b25aa85e7536fa38f3
KieranChambers <120629054+KieranChambers@users.noreply.github.com>
2023-12-20 20:13:10 +0100

    feat: Implement getMainObject function and move it to it's own file

---
+++ utils/getMainObject.js
@@ -0,0 +1,60 @@
+const { ButtonStyle } = require("discord.js");
+const { generatePassphrase } = require("./utilFunctions");
+const { wowWords } = require("./loadJson.js");
+
+function getMainObject(interaction) {
+    const mainObject = {
+        roles: {
+            Tank: {
+                spots: [],
+                style: ButtonStyle.Secondary,
+                disabled: false,
+                customId: "Tank",
+                emoji: `<:tankrole:1181327150708686848>`,
+            },
+            Healer: {
+                spots: [],
+                style: ButtonStyle.Secondary,
+                disabled: false,
+                customId: "Healer",
+                emoji: `<:healrole:1181327153749561364>`,
+            },
+            DPS: {
+                spots: [],
+                style: ButtonStyle.Secondary,
+                disabled: false,
+                customId: "DPS",
+                emoji: `<:dpsrole:1181327148624117870>`,
+            },
+            DPS2: {
+                customId: "DPS2",
+                emoji: `<:dpsrole:1181327148624117870>`,
+            },
+            DPS3: {
+                customId: "DPS3",
+                emoji: `<:dpsrole:1181327148624117870>`,
+            },
+        },
+        embedData: {
+            dungeonName: "",
+            dungeonDifficulty: "",
+            listedAs: "",
+            spotIcons: [],
+            filledSpot: "~~Filled Spot~~",
+        },
+        interactionId: interaction.id,
+        interactionUser: {
+            userId: `<@${interaction.user.id}>`,
+            userChosenRole: "",
+        },
+        utils: {
+            passphrase: {
+                phrase: generatePassphrase(wowWords),
+            },
+        },
+    };
+
+    return mainObject;
+}
+
+module.exports = { getMainObject };

f258edd0a654a4448817964eabeb2a7fae4ee716
KieranChambers <120629054+KieranChambers@users.noreply.github.com>
2024-10-10 07:59:19 +0100

    chore: Update eslin & package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,25 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-    {
-        // Apply CommonJS settings to all JavaScript files
-        files: ["**/*.js"],
-        languageOptions: {
-            sourceType: "commonjs",
-        },
-        env: {
-            node: true, // Enable Node.js environment
-        },
-    },
-    {
-        // General settings for all files
-        languageOptions: {
-            globals: {
-                ...globals.browser, // Use browser globals
-                ...globals.node, // Add node globals (e.g., __dirname)
-            },
-        },
-    },
-    // Apply the recommended ESLint rules
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
     pluginJs.configs.recommended,
 ];
